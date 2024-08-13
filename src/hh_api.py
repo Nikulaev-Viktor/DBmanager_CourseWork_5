2276f810b50ba0ab1ddaf000120f61b4c980dfc6
Viktor Nikulaev <viktor_1611@mail.ru>
2024-08-13 23:52:18 +0300

    add create db, save vacancies to db, save companies to db

--- src/hh_api.py
+++ src/hh_api.py
@@ -23,7 +23,7 @@ def get_employee():
     employers = []
     for employer_id in employer_ids:
         url_emp = f"https://api.hh.ru/employers/{employer_id}"
-        employer_info = requests.get(url_emp, ).json()
+        employer_info = requests.get(url_emp).json()
         employers.append(employer_info)
 
     return employers
@@ -41,4 +41,75 @@ def get_vacancies():
     return vacancy
 
 
+def create_db(database_name: str, params: dict) -> None:
+    """Создание базы данных для вакансий и компаний"""
 
+    conn = psycopg2.connect(dbname='postgres', **params)
+    conn.autocommit = True
+    cur = conn.cursor()
+    cur.execute(f'DROP DATABASE IF EXISTS {database_name}')
+    cur.execute(f'CREATE DATABASE {database_name}')
+
+    conn.close()
+
+    conn = psycopg2.connect(dbname=database_name, **params)
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE companies(
+            company_id INTEGER PRIMARY KEY,
+            company_name VARCHAR(50) NOT NULL,
+            company_url TEXT
+            )
+        """)
+
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE vacancies(
+            vacancy_id INTEGER PRIMARY KEY,
+            company_id INTEGER REFERENCES companies(company_id),
+            vacancy_name VARCHAR (100) NOT NULL,
+            requirement VARCHAR(255),
+            salary_from INTEGER,
+            salary_to INTEGER,
+            vacancy_url TEXT
+            )
+        """)
+
+
+def save_to_db_companies(companies_data: list[dict[str, Any]], database_name: str, params: dict) -> None:
+    """Сохранение данных о компаниях в базу данных."""
+    conn = psycopg2.connect(dbname=database_name, **params)
+    with conn.cursor() as cur:
+        for company in companies_data:
+            company_id = company['company_id']
+            company_name = company['company_name']
+            company_url = company['company_url']
+            cur.execute("""
+                    INSERT INTO companies (company_id, company_name, company_url)
+                    VALUES (%s, %s, %s)
+                """, (company_id, company_name, company_url))
+    conn.commit()
+    conn.close()
+
+
+def save_to_db_vacancies(vacancies_data: list[dict[str, Any]], database_name: str, params: dict) -> None:
+    """Сохранение данных о вакансиях в базу данных."""
+    conn = psycopg2.connect(dbname=database_name, **params)
+    with conn.cursor() as cur:
+        for vacancy in vacancies_data:
+            vacancy_id = vacancy['id']
+            company_id = vacancy['employer']['id']
+            vacancy_name = vacancy['name']
+            requirement = vacancy['snippet'].get('requirement', None)
+            salary = vacancy['salary']
+            salary_from = salary.get('from') if salary else None
+            salary_to = salary.get('to') if salary else None
+            vacancy_url = vacancy['alternate_url']
+            cur.execute("""
+                INSERT INTO vacancies (vacancy_id, company_id, vacancy_name, requirement, salary_from, salary_to, 
+                vacancy_url)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """, (vacancy_id, company_id, vacancy_name, requirement, salary_from, salary_to,
+                  vacancy_url))
+    conn.commit()
+    conn.close()
